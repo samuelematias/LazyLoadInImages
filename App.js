41035731cfc6497b46db6615498c80bcf6d7dc16
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2019-01-12 19:01:26 -0300

    improving the code.

--- App.js
+++ App.js
@@ -2,16 +2,37 @@
 import React, { Component } from 'react';
 
 /* Presentational */
-import { View, Image, Animated } from 'react-native';
+import { View, Image, Animated, FlatList } from 'react-native';
 
 export default class App extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
-			opacity: new Animated.Value(0)
+			opacity: new Animated.Value(0),
+			data: [
+				{
+					id: 0,
+					url: 'https://images.unsplash.com/photo-1467703834117-04386e3dadd8',
+					loaded: false
+				},
+				{
+					id: 1,
+					url: 'https://images.unsplash.com/photo-1511971523672-53e6411f62b9',
+					loaded: false
+				},
+				{
+					id: 2,
+					url: 'https://images.unsplash.com/photo-1494522358652-f30e61a60313',
+					loaded: false
+				}
+			]
 		};
 	}
+
+	/**
+	 * Example 1
+	 */
 
 	onLoad = event => {
 		Animated.timing(this.state.opacity, {
@@ -20,7 +41,7 @@ export default class App extends Component {
 		}).start();
 	};
 
-	render() {
+	_renderSingleImage = () => {
 		return (
 			<View
 				style={{
@@ -62,5 +83,54 @@ export default class App extends Component {
 				/>
 			</View>
 		);
+	};
+
+	/**
+	 * Example 1 END
+	 */
+
+	/**
+	 * Example 2
+	 */
+
+	_handleLazyLoad = ({ viewableItems }) => {
+		const newData = this.state.data.map(image =>
+			viewableItems.find(({ item }) => item.id === image.id)
+				? { ...image, loaded: true }
+				: image
+		);
+
+		this.setState({ data: newData });
+	};
+
+	_renderItem = ({ item }) => (
+		<View style={{ marginVertical: 20, height: 300, backgroundColor: '#EEE' }}>
+			{item.loaded && (
+				<Image
+					source={{ uri: item.url }}
+					style={{ width: '100%', height: 300 }}
+				/>
+			)}
+		</View>
+	);
+
+	_renderImageList = () => {
+		return (
+			<FlatList
+				data={this.state.data}
+				renderItem={this._renderItem}
+				keyExtractor={item => item.id.toString()}
+				onViewableItemsChanged={this._handleLazyLoad}
+			/>
+		);
+	};
+
+	/**
+	 * Example 2 END
+	 */
+
+	render() {
+		// return this._renderSingleImage();
+		return this._renderImageList();
 	}
 }
