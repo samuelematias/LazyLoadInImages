c64238c47f808d1e45923416a51fdb324a52ca25
Samuel Mataraso <samuelmataraso@MacBook-Air-de-Samuel-Mataraso.local>
2019-01-12 18:44:32 -0300

    init project.

---
+++ App.js
@@ -0,0 +1,62 @@
+/* Core */
+import React, { Component } from 'react';
+
+/* Presentational */
+import { View, Image, Animated } from 'react-native';
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			opacity: new Animated.Value(0)
+		};
+	}
+
+	onLoad = event => {
+		Animated.timing(this.state.opacity, {
+			toValue: 1,
+			duration: 300
+		}).start();
+	};
+
+	render() {
+		return (
+			<View
+				style={{
+					// backgroundColor: '#EEE',
+					width: '100%',
+					height: 300 || '100%',
+					alignItems: 'center',
+					justifyContent: 'center',
+					marginTop: 20
+				}}
+			>
+				<Image
+					source={require('./placeholder.png')}
+					// source={{
+					// 	uri: 'https://images.unsplash.com/photo-1467703834117-04386e3dadd8'
+					// }}
+					style={{ width: '100%', height: 300 || '100%' }}
+				/>
+				<Animated.Image
+					style={[
+						this.props.style,
+						{
+							position: 'absolute',
+							opacity: this.state.opacity,
+							width: '100%',
+							height: 300 || '100%'
+						}
+					]}
+					// source={require('./pp.jpeg')}
+					source={{
+						uri:
+							'https://imgix.ranker.com/user_node_img/50081/1001610901/original/people-find-me-interesting-photo-u2?w=650&q=50&fm=jpg&fit=crop&crop=faces'
+					}}
+					onLoad={this.onLoad}
+				/>
+			</View>
+		);
+	}
+}
